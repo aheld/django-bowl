51ddca01eec71cc8741a4e626035e43dce5a817c
aheld <aarondheld@gmail.com>
2015-02-07 14:02:45 -0500

    week 1 checkpoint

--- bowling/score/models.py
+++ bowling/score/models.py
@@ -3,8 +3,32 @@ from django.db import models
 # Create your models here.
 
 class Game():
+    def __init__(self):
+        self.rolls = []
+
     def roll(self, pins):
-        pass
+        self.rolls.append(pins)
 
     def score(self):
-        return -1
+        idx = 0
+        frame_count=0
+        score_sum=0
+        while True:
+            #check strike
+            if (self.rolls[idx]) == 10:
+                score_sum+=10 + self.rolls[idx+1] + self.rolls[idx+2] 
+                idx += 1
+            #check spare
+            elif (self.rolls[idx] + self.rolls[idx+1]) == 10:
+                score_sum+=10 + self.rolls[idx+2] 
+                idx += 2
+            #sum roll
+            else:
+                score_sum += self.rolls[idx]    
+                score_sum += self.rolls[idx+1]    
+                idx += 2
+
+            frame_count += 1
+            if frame_count == 10:
+                break
+        return score_sum
--- bowling/score/tests.py
+++ bowling/score/tests.py
@@ -1,14 +1,42 @@
 from django.test import TestCase
 
-from score.models import Game as Kevin
+from score.models import Game
 
 class BowlingTest(TestCase):
     def setUp(self):
-        pass
+        self.game = Game()
+
+    def roll_many(self,pins,rolls=20):
+        for i in range(rolls):
+            self.game.roll(pins)
 
     def test_gutter_game(self):
-        game = Kevin()
-        for i in range(20):
-            game.roll(0)
+        self.roll_many(0)
+        self.assertEqual(self.game.score(), 0)
 
-        self.assertEqual(game.score(), 0)
+    def test_simple_game(self):
+        self.roll_many(1)
+        self.assertEqual(self.game.score(), 20)
+
+    def test_sum_game(self):
+        self.roll_many(3, rolls=10)
+        self.roll_many(4, rolls=10)
+        self.assertEqual(self.game.score(), 70)
+
+    def test_spare_game(self):
+        self.roll_many(5, rolls=3)
+        self.roll_many(0, rolls=17)
+        self.assertEqual(self.game.score(), 20)
+
+    def test_spare_game(self):
+        self.roll_many(5, rolls=3)
+        self.roll_many(0, rolls=1)
+        self.roll_many(5, rolls=3)
+        self.roll_many(0, rolls=13)
+        self.assertEqual(self.game.score(), 40)
+
+    def test_strike_game(self):
+        self.roll_many(10, rolls=1)
+        self.roll_many(3, rolls=2)
+        self.roll_many(0, rolls=16)
+        self.assertEqual(self.game.score(),22)
